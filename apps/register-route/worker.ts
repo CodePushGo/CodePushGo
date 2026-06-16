b6ff21787192c3b7250dc34a0babdc0099ea8993
Martin Donadieu <martindonadieu@gmail.com>
2026-06-16 17:05:27 +0200

    Serve website register route

--- apps/register-route/worker.ts
+++ apps/register-route/worker.ts
@@ -1,34 +1,13 @@
-const consoleOrigin = 'https://console.codepushgo.com'
-const assetPrefix = '/register-assets/'
+const websiteOrigin = 'https://codepushgo-website.pages.dev'
 
-function rewriteToConsole(request: Request) {
+function toWebsiteRegister(request: Request) {
   const incoming = new URL(request.url)
-  const pathname = incoming.pathname.startsWith(assetPrefix)
-    ? `/assets/${incoming.pathname.slice(assetPrefix.length)}`
-    : incoming.pathname
-  const target = new URL(pathname + incoming.search, consoleOrigin)
+  const target = new URL('/register/' + incoming.search, websiteOrigin)
   return new Request(target, request)
-}
-
-function rewriteAssetUrls(html: string) {
-  return html
-    .replaceAll('src="/assets/', `src="${assetPrefix}`)
-    .replaceAll('href="/assets/', `href="${assetPrefix}`)
 }
 
 export default {
   async fetch(request: Request) {
-    const response = await fetch(rewriteToConsole(request))
-    const contentType = response.headers.get('content-type') || ''
-    if (!contentType.includes('text/html'))
-      return response
-
-    const headers = new Headers(response.headers)
-    headers.delete('content-length')
-    return new Response(rewriteAssetUrls(await response.text()), {
-      status: response.status,
-      statusText: response.statusText,
-      headers,
-    })
+    return fetch(toWebsiteRegister(request))
   },
 }
